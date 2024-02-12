e211bb4f29293afc7fd2f038ca2133902dd9ac44
Natashi <32347635+Natashi@users.noreply.github.com>
2024-02-12 22:15:14 +0700

    Rename make_sets.sh to run_generator.sh

--- data/run_generator.sh
+++ data/run_generator.sh
@@ -10,8 +10,8 @@ path_generator=$1
 
 declare -rA amounts=(
 	#["1e3"]=1024
-	#["2e3"]=2000
-	#["5e3"]=5000
+	["2e3"]=2000
+	["5e3"]=5000
 	["1e4"]=10000
 	["2e4"]=20000
 	["5e4"]=50000
@@ -22,8 +22,8 @@ declare -rA amounts=(
 	["2e6"]=2000000
 	["5e6"]=5000000
 	["1e7"]=10000000
-	#["2e7"]=20000000
-	#["5e7"]=50000000
+	["2e7"]=20000000
+	["5e7"]=50000000
 	#["1e8"]=100000000
 	#["2e8"]=200000000
 	#["5e8"]=500000000
@@ -31,18 +31,18 @@ declare -rA amounts=(
 )
 
 declare -ra types=(
-	"i32"
-	"i64"
-	#"u32"
-	#"u64"
+	#"i32"
+	#"i64"
+	"u32"
+	"u64"
 	"f64"
 )
 
 declare -rA arranges=(
 	["ran"]="random"
-	["rev"]="reversed"
-	["fu"]="fewunique"
-	["ns"]="nsorted"
+	#["rev"]="reversed"
+	#["fu"]="fewunique"
+	#["ns"]="nsorted"
 )
 
 for num_str in "${!amounts[@]}"
